b220a6c9b9a9f8642da616f55bf4b2752a3285d7
bgarciama <blanca.gar.man@gmail.com>
2024-02-14 17:01:20 -0600

    cambio en script invluye funciones

--- src/script.py
+++ src/script.py
@@ -1,4 +1,20 @@
 #script.py
 '''Funciones que luego puedas importar a tu código principal (prep.py, train.py, inference.py) 
 '''
+#Importa librerías
+import pandas as pd 
+import numpy as np
+import matplotlib.pyplot as plt
+import seaborn as sns
+from sklearn.model_selection import train_test_split
+from sklearn.linear_model import LinearRegression
+from sklearn.preprocessing import StandardScaler
+from sklearn.ensemble import RandomForestRegressor
+from sklearn.metrics import mean_squared_error, mean_absolute_error, r2_score
+from sklearn.model_selection import GridSearchCV
+from sklearn.impute import IterativeImputer
+from sklearn.metrics import r2_score, mean_absolute_error, mean_squared_error
+from sklearn.compose import ColumnTransformer
+from sklearn.preprocessing import LabelEncoder 
 
+def descargar_datos(data_train,data_test):
