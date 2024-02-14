ccd48efc0c44b95c59705ec2a9f44d4a5a51c982
bgarciama <blanca.gar.man@gmail.com>
2024-02-14 12:07:03 -0600

    incorpora datos

---
+++ prep.py
@@ -0,0 +1,12 @@
+# prep.py
+
+def descargar_datos(data_train,data_test):
+    '''Descaga la info que vendrá de CSV del data raw
+    Params:
+        data_train: informacion del dataset de entrenamiento
+        data_test:  informacion del dataset de test
+    Returns:
+        Un dataframe con las dos bases unidas
+    '''
+    return data_total
+
