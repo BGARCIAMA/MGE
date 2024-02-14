07d26457682f6e3f2a19ffeb08e99ab662533af3
bgarciama <blanca.gar.man@gmail.com>
2024-02-14 13:34:14 -0600

    Modulado de código

--- prep.py
+++ prep.py
@@ -1,4 +1,25 @@
 # prep.py
+'''Este script prepara los datos para incorporarlos al modelo
+    Las funciones dentro de este script son:
+    - descargar_datos
+    - impute_continuous_missing_data
+    - preprocesar_datos
+'''
+#Importa librerías
+import pandas as pd 
+import numpy as np
+import matplotlib.pyplot as plt
+import seaborn as sns
+from sklearn.model_selection import train_test_split
+from sklearn.linear_model import LinearRegression
+from sklearn.preprocessing import StandardScaler
+from sklearn.ensemble import RandomForestRegressor
+from sklearn.metrics import mean_squared_error, mean_absolute_error, r2_score
+from sklearn.model_selection import GridSearchCV
+from sklearn.impute import IterativeImputer
+from sklearn.metrics import r2_score, mean_absolute_error, mean_squared_error
+from sklearn.compose import ColumnTransformer
+
 
 def descargar_datos(data_train,data_test):
     '''Descaga la info que vendrá de CSV del data raw
@@ -8,5 +29,151 @@ def descargar_datos(data_train,data_test):
     Returns:
         Un dataframe con las dos bases unidas
     '''
+    #Lee los archivos csv con los datos
+    data_train = pd.read_csv("train.csv")
+    data_test = pd.read_csv("test.csv")
+
+    #Quita las columnas de ID
+    train_sin_ID = data_train.drop('Id', axis=1)
+    test_sin_ID = data_test.drop('Id', axis=1)
+
+    #Une las bases para la base total
+    data_total = pd.concat([train_sin_ID, test_sin_ID], axis = 0)
+
     return data_total
 
+def impute_continuous_missing_data(passed_col):
+    '''Completa las variables vacías usando un imputador (apoyándonos del código
+    https://www.kaggle.com/code/muhammadibrahimqasmi/predicting-house-prices")
+    '''
+    bool_cols = []
+    numeric_cols = ['MSSubClass',
+    'LotFrontage',
+    'LotArea',
+    'OverallQual',
+    'OverallCond',
+    'YearBuilt',
+    'YearRemodAdd',
+    'MasVnrArea',
+    'BsmtFinSF1',
+    'BsmtFinSF2',
+    'BsmtUnfSF',
+    'TotalBsmtSF',
+    '1stFlrSF',
+    '2ndFlrSF',
+    'LowQualFinSF',
+    'GrLivArea',
+    'BsmtFullBath',
+    'BsmtHalfBath',
+    'FullBath',
+    'HalfBath',
+    'BedroomAbvGr',
+    'KitchenAbvGr',
+    'TotRmsAbvGrd',
+    'Fireplaces',
+    'GarageYrBlt',
+    'GarageCars',
+    'GarageArea',
+    'WoodDeckSF',
+    'OpenPorchSF',
+    'EnclosedPorch',
+    '3SsnPorch',
+    'ScreenPorch',
+    'PoolArea',
+    'MiscVal',
+    'MoSold',
+    'YrSold',
+    'SalePrice']
+
+    df_null = var_modelo[var_modelo[passed_col].isnull()]
+    df_not_null = var_modelo[var_modelo[passed_col].notnull()]
+
+    X = df_not_null.drop(passed_col, axis=1)
+    y = df_not_null[passed_col]
+    
+    other_missing_cols = [col for col in missingdata if col != passed_col]
+    
+    label_encoder = LabelEncoder()
+
+    for col in X.columns:
+        if X[col].dtype == 'object' or X[col].dtype == 'category':
+            X[col] = label_encoder.fit_transform(X[col])
+    
+    iterative_imputer = IterativeImputer(estimator=RandomForestRegressor(random_state = 123), add_indicator=True)
+
+    for col in other_missing_cols:
+        if X[col].isnull().sum() > 0:
+            col_with_missing_values = X[col].values.reshape(-1, 1)
+            imputed_values = iterative_imputer.fit_transform(col_with_missing_values)
+            X[col] = imputed_values[:, 0]
+        else:
+            pass
+    
+    X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state = 123)
+
+    rf_regressor = RandomForestRegressor()
+
+    rf_regressor.fit(X_train, y_train)
+
+    y_pred = rf_regressor.predict(X_test)
+
+    print("MAE =", mean_absolute_error(y_test, y_pred), "\n")
+    print("RMSE =", mean_squared_error(y_test, y_pred, squared=False), "\n")
+    print("R2 =", r2_score(y_test, y_pred), "\n")
+
+    X = df_null.drop(passed_col, axis=1)
+
+    for col in X.columns:
+        if X[col].dtype == 'object' or X[col].dtype == 'category':
+            X[col] = label_encoder.fit_transform(X[col])
+
+    for col in other_missing_cols:
+        if X[col].isnull().sum() > 0:
+            col_with_missing_values = X[col].values.reshape(-1, 1)
+            imputed_values = iterative_imputer.fit_transform(col_with_missing_values)
+            X[col] = imputed_values[:, 0]
+        else:
+            pass
+                
+    if len(df_null) > 0: 
+        df_null[passed_col] = rf_regressor.predict(X)
+    else:
+        pass
+
+    df_combined = pd.concat([df_not_null, df_null])
+
+    return df_combined[passed_col]
+
+def preprocesar_datos(data_total):
+    '''Trabaja en la base total para preprocesar
+    Params:
+        data_total: base completa para prepocesar las variables para modelo
+    Returns:
+        Un dataframe con las variables finales a utilizar en el modelo
+    '''
+    #Quita variables con muchos missings
+    var_sin_miss = data_total.drop(['PoolQC', 'MiscFeature', 'Alley', 'Fence', 'FireplaceQu'], axis = 1)
+
+    #Se queda con variables numericas solamente
+    var_modelo = var_sin_miss.select_dtypes(include = ['float64', 'int64'])
+
+
+    #Elimina filas y columas duplicadas
+    var_modelo = var_modelo.drop_duplicates()
+    var_modelo = var_modelo.reset_index(drop = True)
+    
+    import warnings
+    warnings.filterwarnings('ignore')
+
+    # Imputamos informacioón en valores vacíos con nuestras funciones
+    for col in missingdata:
+        print("Missing Values", col, ":", str(round((var_modelo[col].isnull().sum() / len(var_modelo)) * 100, 2))+"%")
+        if col in numeric_cols:
+            var_modelo[col] = impute_continuous_missing_data(col)
+        else:
+            pass
+   
+    data_final = var_modelo[['OverallQual', 'YearBuilt', 'YearRemodAdd', 'LotFrontage', 
+                            'TotalBsmtSF', 'GrLivArea', 'GarageArea', 'SalePrice']]
+
+    return data_final
---
+++ train.py
@@ -0,0 +1,70 @@
+# train.py
+'''Este script entrena el modelo con los datos de prep
+'''
+#Importa librerías
+import pandas as pd 
+import numpy as np
+import matplotlib.pyplot as plt
+import seaborn as sns
+from sklearn.model_selection import train_test_split
+from sklearn.linear_model import LinearRegression
+from sklearn.preprocessing import StandardScaler
+from sklearn.ensemble import RandomForestRegressor
+from sklearn.metrics import mean_squared_error, mean_absolute_error, r2_score
+from sklearn.model_selection import GridSearchCV
+from sklearn.impute import IterativeImputer
+from sklearn.metrics import r2_score, mean_absolute_error, mean_squared_error
+from sklearn.compose import ColumnTransformer
+
+def entrena_modelo(data_final):
+    # Entrenamos el modelo 
+    # Seleccionar características numéricas y eliminar 'SalePrice'
+    numerical_cols = data_final.select_dtypes(include=['int64', 'float64']).drop('SalePrice', axis=1).columns
+    numerical_transformer = StandardScaler()
+
+    # Crear ColumnTransformer
+    preprocessor = ColumnTransformer(
+        transformers=[
+            ('num', numerical_transformer, numerical_cols)
+        ])
+
+    # Modelos
+    model_list = {
+        'Linear Regression': LinearRegression(),
+        'Random Forest Regression': RandomForestRegressor()
+    }
+
+    # División de la base de datos
+    x = data_final.drop('SalePrice', axis=1)
+    y = data_final['SalePrice']
+    x_train, x_test, y_train, y_test = train_test_split(x, y, test_size=0.2, random_state=123)
+
+    # Crear pipelines
+    pipelines = {name: Pipeline(steps=[('preprocessor', preprocessor), ('model', model)]) for name, model in model_list.items()}
+
+    # Entrenamiento y evaluación de los modelos
+    rmse_results = {}
+
+    for name, pipeline in pipelines.items():
+        pipeline.fit(x_train, y_train)
+        predictions = pipeline.predict(x_test)
+        rmse = np.sqrt(mean_squared_error(y_test, predictions))
+        rmse_results[name] = rmse
+
+    # Mostrar los RMSE resultantes
+    rmse_results_sorted = dict(sorted(rmse_results.items(), key=lambda item: item[1]))
+
+    # Se dividen las bases
+    df_train, df_test = train_test_split(data_final, test_size=0.2, random_state=42)
+
+    x_train = df_train.drop(['SalePrice'], axis = 1)
+    y_train = df_train['SalePrice']  
+
+    # Se entrena con el XGBoost Regressor
+    RFR_model = RandomForestRegressor()
+    RFR_model.fit(x_train, y_train)
+
+    # Se hace la predicción en la base test usando el modelo entrenado
+    y_pred = RFR_model.predict(df_test.drop(['SalePrice'], axis = 1))
+
+    joblib.dump(RFR_model, "./rfr_model.joblib")
