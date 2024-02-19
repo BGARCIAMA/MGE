068114b4e75e22791f7e13f7506c86c246cc7550
bgarciama <blanca.gar.man@gmail.com>
2024-02-18 18:04:43 -0600

    correcciones con flake8

--- prep.py
+++ prep.py
@@ -11,10 +11,10 @@ from src.script import descargar_datos, preprocesar_datos
 BASE_PATH_DATA = "./data/raw/"
 PATH_DATA = "./data/raw/data_total.csv"
 PATH_OUT = "./data/raw/"
-BASE_PATH_OUT_PREP="data/prep/data_prep.csv"
+BASE_PATH_OUT_PREP = "data/prep/data_prep.csv"
 
-#Descarga los datos
+# Descarga los datos
 data_total = descargar_datos(BASE_PATH_DATA, PATH_OUT)
 
-#Preprocesa los datos
+# Preprocesa los datos
 data_final = preprocesar_datos(PATH_DATA, BASE_PATH_OUT_PREP)
